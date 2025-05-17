56bbad87121893da16858965d4542e0bec7350bf
changs97 <kin2027@gmail.com>
2025-05-18 05:07:40 +0900

    fix: 스플래시 로직 수정

--- app/src/main/java/com/ecopedia/ecopedia_android/presentation/SplashActivity.kt
+++ app/src/main/java/com/ecopedia/ecopedia_android/presentation/SplashActivity.kt
@@ -2,15 +2,15 @@ package com.ecopedia.ecopedia_android.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.ecopedia.ecopedia_android.R
 import com.ecopedia.ecopedia_android.data.source.local.TokenManager
 import com.ecopedia.ecopedia_android.presentation.signin.ui.SignInActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -22,18 +22,18 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-    }
+        lifecycleScope.launch {
+            delay(1000)
 
-    override fun onResume() {
-        super.onResume()
-        runBlocking {
-            delay(2000)
             val token = tokenManager.getAccessToken().firstOrNull()
-            Log.d("TEST", "token ${token}")
-            val intent = if (token != null) Intent(this@SplashActivity, MainActivity::class.java)
-            else Intent(this@SplashActivity, SignInActivity::class.java)
+            val intent = if (token != null) {
+                Intent(this@SplashActivity, MainActivity::class.java)
+            } else {
+                Intent(this@SplashActivity, SignInActivity::class.java)
+            }
 
             startActivity(intent)
+            finish() // SplashActivity 종료
         }
     }
 }
